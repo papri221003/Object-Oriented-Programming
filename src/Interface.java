c3f533408c7538c6cf00f50ad4ba183a102318db
papri221003 <bhpapri03@gmail.com>
2025-09-09 13:33:30 +0530

    Initial commit

---
+++ src/Interface.java
@@ -0,0 +1,32 @@
+interface animal{
+    public void speak();
+    public void eat();
+}
+class dog implements animal{
+    public void speak()
+    {
+        System.out.println("Dog can bark");
+    }
+    public void eat(){
+        System.out.println("Dog like to eat meat");
+    }
+}
+class cat implements animal{
+    public void speak()
+    {
+        System.out.println("Cat can meow");
+    }
+    public void eat(){
+        System.out.println("Cat like to eat fish and milk");
+    }
+}
+public class Interface {
+    public static void main(String[] args) {
+        animal d1=new dog();
+        animal c1=new cat();
+
+        d1.eat();
+        c1.speak();
+
+    }
+}
